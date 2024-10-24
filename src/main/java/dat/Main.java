6f0d97b07dda428e967d903d92ccb23ea308ce0c
MateenCPH <cph-mr645@cphbusiness.dk>
2024-10-24 20:22:50 +0200

    changed port

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -9,7 +9,7 @@ import jakarta.persistence.EntityManagerFactory;
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationConfig.startServer(7070);
+        ApplicationConfig.startServer(7007);
         Populate.main(args);
         //New test
         //new test again
